1588a11969a6dbb8dec6adeda7f08c0c1ac7fae6
Aidan Starr <aidan.starr19@gmail.com>
2020-10-17 18:01:42 -0400

    Updates

---
+++ precache-manifest.ae3bcde5da9ebc2b761860759661c878.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9b839cf7ad8ae9afe6d623b6a5393b49",
+    "url": "/BudgetApp/index.html"
+  },
+  {
+    "revision": "b5a1826ce7362cbdd599",
+    "url": "/BudgetApp/static/css/main.ca1c2687.chunk.css"
+  },
+  {
+    "revision": "d3b0f6dcdda9e66023f3",
+    "url": "/BudgetApp/static/js/2.2cd43574.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/BudgetApp/static/js/2.2cd43574.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b5a1826ce7362cbdd599",
+    "url": "/BudgetApp/static/js/main.8c3d37da.chunk.js"
+  },
+  {
+    "revision": "2e23f59c12f7565e6f99",
+    "url": "/BudgetApp/static/js/runtime-main.ec138f8f.js"
+  }
+]);
