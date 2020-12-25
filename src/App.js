afea3d4a5927e47d2cba70abb2b82b0d9a695347
Aidan Starr <aidan.starr19@gmail.com>
2020-12-25 12:33:55 -0500

    chart doesn't change position based on list length anymore

--- src/App.js
+++ src/App.js
@@ -117,9 +117,9 @@ function App() {
   return (
     <div className='container'>
       <div className="monthDisplay">
-        {prevCheck && <button disabled={!prevCheck} onClick={()=>setMonth(lastMonth)}>Last Month</button>}
+        {prevCheck && <button onClick={()=>setMonth(lastMonth)}>Last Month</button>}
         <h1>{month}</h1>
-        {nextCheck && <button disabled={!nextCheck} onClick={()=>setMonth(nextMonth)}>Next Month</button>}
+        {nextCheck && <button onClick={()=>setMonth(nextMonth)}>Next Month</button>}
       </div>
       <div className='incomeExpense'>
         <div className='income'>
